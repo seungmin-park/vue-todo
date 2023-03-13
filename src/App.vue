61105017dac328f7fdd6f0584bcddfe53f7dcad8
박승민 <tmddudals369@naver.com>
2023-03-13 15:40:33 +0900

    feat: 할 일 목록 전부 삭제 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
     <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import TodoHeader from "./components/TodoHeader.vue";
 import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
   components: {
     "TodoHeader": TodoHeader,
     "TodoInput": TodoInput,
-    "TodoList": TodoList
+    "TodoList": TodoList,
+    "TodoFooter": TodoFooter,
   }
 }
 </script>
---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="clearAllContainer">
+  <span class="clearBtn" @click="clearTodo">Clear All</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: "TodoFooter",
+  methods: {
+    clearTodo() {
+      localStorage.clear();
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
