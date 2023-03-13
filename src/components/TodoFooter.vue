c0de1e90da399bb25f197560837d7ff86b1221b1
박승민 <tmddudals369@naver.com>
2023-03-13 15:43:57 +0900

    style: 할 일 전부 삭제 기능 마크업

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -16,5 +16,17 @@ export default {
 </script>
 
 <style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
 
+.clearBtn {
+  color: #e20303;
+  display: block;
+}
 </style>
