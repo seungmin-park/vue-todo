adb757191b1bb647a64dd74c96d5b72d46424002
박승민 <tmddudals369@naver.com>
2023-03-13 15:28:23 +0900

    feat: 할 일 목록 & 삭제 버튼 마크업

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,12 @@
 <template>
   <section>
     <ul>
-      <li v-for="todoItem in todoItems">
+      <li v-for="todoItem in todoItems" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
         {{ todoItem }}
+        <span class="removeBtn" type="button">
+          <i class="fa fa-trash-o" aria-hidden="true"></i>
+        </span>
       </li>
     </ul>
   </section>
@@ -29,4 +33,32 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
