fcc1046bfef4be4e5c5e3f25d8bfb918ab73543a
박승민 <tmddudals369@naver.com>
2023-03-13 15:27:35 +0900

    feat: 로컬 스토리지 데이터 -> 뷰 데이터 저장

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      todoItems: [],
+    };
+  },
+
+  created() {
+    // 로컬 스토리지 데이터 -> 뷰 데이터 저장
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
