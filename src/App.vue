4d8987aaf94b9d41f2b923da63a50026dc0f117f
박승민 <tmddudals369@naver.com>
2023-03-13 15:02:21 +0900

    feat: 할 일 입력 및 저장 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
   </div>
 </template>
 
 <script>
 import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
 
 export default {
   components: {
     "TodoHeader": TodoHeader,
+    "TodoInput": TodoInput,
   }
 }
 </script>
@@ -18,5 +21,18 @@ export default {
 body {
   text-align: center;
   background-color: #F6F6F8;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
