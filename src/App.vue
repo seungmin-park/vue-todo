17629f853543531923a8c401640714c9b0d943f6
박승민 <tmddudals369@naver.com>
2023-03-13 16:24:06 +0900

    fix: 할 일 목록 전부 삭제시 목록이 갱신되지 않는 문제 해결

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <TodoHeader></TodoHeader>
     <TodoInput v-on:addTodo="addTodo"></TodoInput>
     <TodoList v-bind:propsdata="todoItems"></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -31,6 +31,10 @@ export default {
     addTodo(todoItem) {
       localStorage.setItem(todoItem, todoItem);
       this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
     },
   },
   components: {
